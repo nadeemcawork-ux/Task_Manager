3e233a5300a58af397648f3191fc9bc2f6489bf8
nadeemcawork-ux <nadeem.ca.work@gmail.com>
2025-11-06 20:09:30 +0400

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc' // 👈 use -swc here
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/Task_Manager/', // 👈 this now works correctly
-})
-

2976f3265373c7389fb8b715f4cd534d4a719eba
nadeemcawork-ux <nadeem.ca.work@gmail.com>
2025-11-06 20:25:48 +0400

    done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Task_Manager/', // 👈 Must match your repo name exactly
 })
